05b6f7950cd20de1fe98002ab041ebbb0651b16d
Md Wasim Aslam <wasimaslam2897@gmail..com>
2023-10-13 22:28:17 +0530

    UI Updated

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,18 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import './Home.css'; // Import your CSS file
-
 import { createClient } from '@supabase/supabase-js'
+import gptLogo from "../assets/chatgpt.svg";
+import addBtn from "../assets/add-30.png";
+import messageIcon from "../assets/message.svg";
+import HomeIcon from "../assets/home.svg"
+import Saved from "../assets/bookmark.svg"
+import UpgradeIcon from "../assets/rocket.svg"
+import SendIcon from "../assets/send.svg"
+import UserIcon from "../assets/user-icon.png"
+import gptIcon from "../assets/chatgptLogo.svg"
 
-// Use a custom domain as the supabase URL
+
 
 const supabaseUrl = 'https://fabwcdsfiwzytkrpuijy.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZhYndjZHNmaXd6eXRrcnB1aWp5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTcwNTM3MTIsImV4cCI6MjAxMjYyOTcxMn0._PqpB6HLSmiN65Wk_-ts-_z8Vn6R3DCvdBu_fS49ztg'
@@ -163,22 +171,91 @@ listAllBucxkets()
   };
 
   return (
-    <div className="container">
-      <h1>Chat with PDF</h1>
-      <label htmlFor="myfile">Select a file:</label>
-      <input
-        type="file"
-        id="myfile"
-        name="myfile"
-        onChange={handleFileChange}
-      />
-      <button className="upload-button" onClick={()=>{uploadFile()}}>
-        Upload File
-      </button>
-      <button className="message-button" onClick={handleMessage}>
-        Message
-      </button>
+   <div className='App'>
+    <div className='sidebar'>
+      <div className='upperSide'>
+        <div className="upperSideTop">
+          <img src={gptLogo} alt='Logo' className='logo' />
+          <span className='brand'>DocuVerse</span>
+          <button className="midBtn">
+            <img src={addBtn} alt="" className="addBtn" />
+            New Chat
+          </button>
+          <div className="upperSideBottom">
+            <button className="query">
+            <img src={messageIcon} alt="Query" className="uploadBtn" />
+            Upload
+            </button>
+          </div>
+        </div>
+
+      </div>
+      <div className='lowerSide'>
+          <div className="listItem"><img src={HomeIcon} alt='' className='listItemImg'/>Home</div>
+          <div className="listItem"><img src={Saved} alt='' className='listItemImg'/>Saved</div>
+          <div className="listItem"><img src={UpgradeIcon} alt='' className='listItemImg'/>Upgrade To Pro</div>
+      </div>
     </div>
+
+
+    {/* Main Chat Section */}
+    <div className='main'>
+      <div className="chats">
+        <div className="chat">
+          <img className='chatImg' src={UserIcon} alt="" /> <p className="txt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate reprehenderit eius laborum nisi nostrum esse nemo voluptatibus! Cum, illo reprehenderit?</p>
+        </div>
+
+        <div className="chat bot">
+          <img className='chatImg' src={gptIcon} alt="" /> <p className="txt">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos, doloremque maiores necessitatibus minus ut eos tenetur animi repellat! Saepe odit, quod, ipsam aperiam accusantium corporis, quia exercitationem qui fugiat molestiae vitae cupiditate neque. Eum ratione est inventore alias distinctio amet nulla saepe laborum placeat ipsum ipsam in necessitatibus eveniet nostrum dolore, dolor quisquam quo ea voluptate enim similique sint recusandae asperiores? Eos ab maxime asperiores! Accusamus rem in quas iure corrupti pariatur, alias illum aliquam unde, aut nihil repudiandae adipisci at eligendi ut repellat, provident quasi eaque aspernatur? Laboriosam nulla placeat qui laudantium commodi veniam sapiente maiores, tenetur facilis unde?</p>
+        </div>
+
+
+
+        <div className="chat">
+          <img className='chatImg' src={UserIcon} alt="" /> <p className="txt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate reprehenderit eius laborum nisi nostrum esse nemo voluptatibus! Cum, illo reprehenderit?</p>
+        </div>
+
+        <div className="chat bot">
+          <img className='chatImg' src={gptIcon} alt="" /> <p className="txt">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos, doloremque maiores necessitatibus minus ut eos tenetur animi repellat! Saepe odit, quod, ipsam aperiam accusantium corporis, quia exercitationem qui fugiat molestiae vitae cupiditate neque. Eum ratione est inventore alias distinctio amet nulla saepe laborum placeat ipsum ipsam in necessitatibus eveniet nostrum dolore, dolor quisquam quo ea voluptate enim similique sint recusandae asperiores? Eos ab maxime asperiores! Accusamus rem in quas iure corrupti pariatur, alias illum aliquam unde, aut nihil repudiandae adipisci at eligendi ut repellat, provident quasi eaque aspernatur? Laboriosam nulla placeat qui laudantium commodi veniam sapiente maiores, tenetur facilis unde?</p>
+        </div> <div className="chat">
+          <img className='chatImg' src={UserIcon} alt="" /> <p className="txt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate reprehenderit eius laborum nisi nostrum esse nemo voluptatibus! Cum, illo reprehenderit?</p>
+        </div>
+
+        <div className="chat bot">
+          <img className='chatImg' src={gptIcon} alt="" /> <p className="txt">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos, doloremque maiores necessitatibus minus ut eos tenetur animi repellat! Saepe odit, quod, ipsam aperiam accusantium corporis, quia exercitationem qui fugiat molestiae vitae cupiditate neque. Eum ratione est inventore alias distinctio amet nulla saepe laborum placeat ipsum ipsam in necessitatibus eveniet nostrum dolore, dolor quisquam quo ea voluptate enim similique sint recusandae asperiores? Eos ab maxime asperiores! Accusamus rem in quas iure corrupti pariatur, alias illum aliquam unde, aut nihil repudiandae adipisci at eligendi ut repellat, provident quasi eaque aspernatur? Laboriosam nulla placeat qui laudantium commodi veniam sapiente maiores, tenetur facilis unde?</p>
+        </div> <div className="chat">
+          <img className='chatImg' src={UserIcon} alt="" /> <p className="txt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate reprehenderit eius laborum nisi nostrum esse nemo voluptatibus! Cum, illo reprehenderit?</p>
+        </div>
+
+        <div className="chat bot">
+          <img className='chatImg' src={gptIcon} alt="" /> <p className="txt">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos, doloremque maiores necessitatibus minus ut eos tenetur animi repellat! Saepe odit, quod, ipsam aperiam accusantium corporis, quia exercitationem qui fugiat molestiae vitae cupiditate neque. Eum ratione est inventore alias distinctio amet nulla saepe laborum placeat ipsum ipsam in necessitatibus eveniet nostrum dolore, dolor quisquam quo ea voluptate enim similique sint recusandae asperiores? Eos ab maxime asperiores! Accusamus rem in quas iure corrupti pariatur, alias illum aliquam unde, aut nihil repudiandae adipisci at eligendi ut repellat, provident quasi eaque aspernatur? Laboriosam nulla placeat qui laudantium commodi veniam sapiente maiores, tenetur facilis unde?</p>
+        </div> <div className="chat">
+          <img className='chatImg' src={UserIcon} alt="" /> <p className="txt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate reprehenderit eius laborum nisi nostrum esse nemo voluptatibus! Cum, illo reprehenderit?</p>
+        </div>
+
+        <div className="chat bot">
+          <img className='chatImg' src={gptIcon} alt="" /> <p className="txt">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos, doloremque maiores necessitatibus minus ut eos tenetur animi repellat! Saepe odit, quod, ipsam aperiam accusantium corporis, quia exercitationem qui fugiat molestiae vitae cupiditate neque. Eum ratione est inventore alias distinctio amet nulla saepe laborum placeat ipsum ipsam in necessitatibus eveniet nostrum dolore, dolor quisquam quo ea voluptate enim similique sint recusandae asperiores? Eos ab maxime asperiores! Accusamus rem in quas iure corrupti pariatur, alias illum aliquam unde, aut nihil repudiandae adipisci at eligendi ut repellat, provident quasi eaque aspernatur? Laboriosam nulla placeat qui laudantium commodi veniam sapiente maiores, tenetur facilis unde?</p>
+        </div> <div className="chat">
+          <img className='chatImg' src={UserIcon} alt="" /> <p className="txt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate reprehenderit eius laborum nisi nostrum esse nemo voluptatibus! Cum, illo reprehenderit?</p>
+        </div>
+
+        <div className="chat bot">
+          <img className='chatImg' src={gptIcon} alt="" /> <p className="txt">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos, doloremque maiores necessitatibus minus ut eos tenetur animi repellat! Saepe odit, quod, ipsam aperiam accusantium corporis, quia exercitationem qui fugiat molestiae vitae cupiditate neque. Eum ratione est inventore alias distinctio amet nulla saepe laborum placeat ipsum ipsam in necessitatibus eveniet nostrum dolore, dolor quisquam quo ea voluptate enim similique sint recusandae asperiores? Eos ab maxime asperiores! Accusamus rem in quas iure corrupti pariatur, alias illum aliquam unde, aut nihil repudiandae adipisci at eligendi ut repellat, provident quasi eaque aspernatur? Laboriosam nulla placeat qui laudantium commodi veniam sapiente maiores, tenetur facilis unde?</p>
+        </div>
+      </div>
+
+
+
+      
+      <div className="chatFooter">
+        <div className="inp">
+          <input type="text" placeholder='Send a message...' /> <button className="send"><img src={SendIcon} className='send' alt='Send'/></button>
+        </div>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem, eaque.</p>
+      </div>
+
+    </div>
+   </div>
   );
 }
 
